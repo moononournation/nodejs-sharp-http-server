e7efefc508267418fde5fcb577d351c3cae9f768
moononournation <moononournation@gmail.com>
2018-01-14 12:40:43 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const sharp = require('sharp');
+
+const photoFolder = './photo/';
+
+//create a server object:
+http.createServer(function (req, res) {
+  let parts = req.url.split('/');
+  let last_part = parts[parts.length - 1];
+  console.log("last_part: [" + last_part + "]");
+
+  if ((last_part) && (last_part > '')) { // return resized image
+    sharp(photoFolder + last_part)
+    .resize(320, 240)
+    .crop()
+    .jpeg()
+    .toBuffer(function (err, data, info) {
+      res.writeHead(200, {'Content-Type': 'image/jpeg'});
+      res.end(data); // Send the file data to the browser.
+    });
+  } else { // return image list
+    let files = fs.readdirSync(photoFolder);
+    let res_str = '';
+    for (var i = 0; i < files.length; i++) {
+      let file = files[i];
+      if (file.substr(-4).toLowerCase() == '.jpg') {
+        if (res_str > '') {
+          res_str += ',';
+        }
+        res_str += file;
+      }
+    }
+    res.end(res_str);
+  }
+
+}).listen(3200);
---
+++ photo/README.md
@@ -0,0 +1,3 @@
+#photo
+
+Copy your photos to this folder. Filename, including extension, should not over 19 characters.
